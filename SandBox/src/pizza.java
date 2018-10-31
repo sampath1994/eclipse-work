2916fa1ec0f776543bf52fb622fe6be809b994f5
promod94 <webpromod@gmail.com>
2018-10-31 23:28:20 +0530

    conflict made

--- SandBox/src/pizza.java
+++ SandBox/src/pizza.java
@@ -3,7 +3,7 @@ public class pizza implements eatable {
 
 	@Override
 	public void eat() {
-		System.out.println("eating with old method!");
+		System.out.println("eating with old methodtytryrtyrey!");
 
 	}
 
